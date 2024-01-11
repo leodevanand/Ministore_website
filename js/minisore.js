ef69383c04dfe830b32a377e36d7dfaf2e3fa9ca
leodevanand <leodevanand1@gmail.com>
2024-01-11 13:07:19 +0530

    first commit

---
+++ js/minisore.js
@@ -0,0 +1,6 @@
+// function navclick(){
+//     document.getElementById("item-icon").style.display = block;
+// }
+// function navclose(){
+//     document.getElementById("item-icon").style.width = "0px";
+// }
